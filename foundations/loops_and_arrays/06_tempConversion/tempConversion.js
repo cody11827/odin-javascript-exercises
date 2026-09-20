0ca1dba9973db88667ac0202b9d550e2666778a1
cody11827 <codytien1@gmail.com>
2026-09-20 10:16:24 -0700

    finish loops and array ex 6

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let newTemp = (temp - 32) / 1.8;
+    return Math.round(newTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    let newTemp = (temp * (1.8)) + 32;
+    return Math.round(newTemp * 10) / 10;
 };
 
 // Do not edit below this line
