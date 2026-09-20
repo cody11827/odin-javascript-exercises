805b3c1833fb8b8def291cfb8429e5f768553149
cody11827 <codytien1@gmail.com>
2026-09-20 10:07:28 -0700

    finish loops and array ex 3

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function(str) {
-    let string = ""
-    for(i = str.length-1; i >= 0; i--){
-        string += str[i];
-    }
-
-    return string;
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,itemToRemove) {
+    return arr.filter((item) => item !== itemToRemove)
 };
 
 // Do not edit below this line
