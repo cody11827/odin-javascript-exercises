74ae4070dfd09e90eb3c3ef48e59f0b1c98f248f
cody11827 <codytien1@gmail.com>
2026-09-20 10:02:48 -0700

    finish loops and arrays ex 2

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let string = ""
+    for(i = str.length-1; i >= 0; i--){
+        string += str[i];
+    }
 
+    return string;
 };
 
 // Do not edit below this line
