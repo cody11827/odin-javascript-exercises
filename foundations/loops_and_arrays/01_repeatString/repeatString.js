54c6a4f800e6189cedab032e8fe83c6c73a91d44
cody11827 <codytien1@gmail.com>
2026-09-20 10:00:28 -0700

    finish loops and arrays ex 1

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str,count) {
+    if (count <= 0){
+        return "ERROR"
+    }
+    let string = ""
+    for(i = 0; i < count;i++){
+        string += str
+    }
+    return string;
 
 };
 
