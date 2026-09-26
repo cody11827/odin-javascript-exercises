40e0a01a57f678ceb592998451803b3454314c7b
cody11827 <codytien1@gmail.com>
2026-09-26 09:25:00 -0700

    finish object basics ex 1

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total,current) => total+current,0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((total,current) => total*current)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a^b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number <= 2){
+    return number;
+  }
+  let total = 1;
+  for(let current = 2; current < number;current++ ){
+    total *=current;
+  }
+  return total;
 };
+
 
 // Do not edit below this line
 module.exports = {
